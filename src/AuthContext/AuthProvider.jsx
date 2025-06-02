5e88fd768821de61232f357227e8df1404889522
ibrahim3761 <mdibrahimabdullah1@gmail.com>
2025-06-02 22:32:10 +0600

    JWT-token-generated

--- src/AuthContext/AuthProvider.jsx
+++ src/AuthContext/AuthProvider.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContext';
 import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
 import { auth } from '../Firebase/firebase.config';
+import axios from 'axios';
 const AuthProvider = ({children}) => {
     const[loading,setLoading] = useState(true);
     const[user,setUser] = useState(null)
@@ -9,6 +10,16 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth,currentUser =>{
             setUser(currentUser)
             setLoading(false)
+            if(currentUser?.email){
+                const userData = {email: currentUser.email}
+                axios.post('http://localhost:3000/jwt',userData)
+                .then(res => {
+                    console.log('token after jwr',res.data);  
+                })
+                .catch(error =>{
+                    console.log(error);
+                })
+            }
         })
         return ()=>{
             unSubscribe();
